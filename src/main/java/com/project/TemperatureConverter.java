c597b155487f090501dfc2fe3fac9b850715969c
José Lázaro <105605569+MarquesLazaro@users.noreply.github.com>
2025-07-04 09:34:52 -0400

    Fix: adicionando o ; que faltava

--- src/main/java/com/project/TemperatureConverter.java
+++ src/main/java/com/project/TemperatureConverter.java
@@ -3,7 +3,7 @@ package com.project;
 public class TemperatureConverter {
     public static double fahrenheitToCelsius(double fahrenheit){
         //return ((fahrenheit - 32) * 5) / 9;
-        return ((fahrenheit - 32) * 9) / 5
+        return ((fahrenheit - 32) * 9) / 5;
     }
 
     public static double celsiusToFahrenheit(double celsius){
